d179b608b071f8331c1f7b881577d85a51c6d74a
Eliasar Gandara <elgandara@csumb.edu>
2016-11-03 16:02:10 -0700

    Set up default Xcode project

---
+++ quickneasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quickneasy
+//
+//  Created by Eliasar Gandara on 11/3/16.
+//  Copyright © 2016 LEA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
