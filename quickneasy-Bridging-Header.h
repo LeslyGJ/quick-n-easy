89b1687f7d799e36c76a9d320e95f5d9c4de26ce
araceli19 <agopar@csumb.edu>
2016-12-14 22:17:05 -0800

    added files for Gmail sign in implementation

---
+++ quickneasy-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  quickneasy-Bridging-Header.h
+//  quickneasy
+//
+//  Created by bear on 12/14/16.
+//  Copyright © 2016 LEA. All rights reserved.
+//
+
+#ifndef quickneasy_Bridging_Header_h
+#define quickneasy_Bridging_Header_h
+#import <Google/SignIn.h>
+
+
+#endif /* quickneasy_Bridging_Header_h */
